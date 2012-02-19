58e0bd99c97d3c05d04c939d9f25946612ec3720
David Stolarsky <david.stolarsky@gmail.com>
2012-02-18 22:20:22 -0500

    safari and firefox too.

--- www/js/script.js
+++ www/js/script.js
@@ -25,6 +25,18 @@
       }
 
 
+function viewport()
+{
+var e = window, a = 'inner';
+if ( !( 'innerWidth' in window ) )
+{
+a = 'client';
+e = document.documentElement || document.body;
+}
+return { width : e[ a+'Width' ] , height : e[ a+'Height' ] }
+}
+
+
       function setState(hash) {
          parts = hash.split('/');
          if(parts.length == 4)
